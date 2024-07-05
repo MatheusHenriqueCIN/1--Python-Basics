0fc5d9d43e674cdd42aecec4188f90f3ccd68e66
MatheusHenriqueCIN <159284886+MatheusHenriqueCIN@users.noreply.github.com>
2024-07-05 02:00:50 -0300

    Update 2-tipos.py

--- 2-tipos.py
+++ 2-tipos.py
@@ -1,13 +1,17 @@
+# Tipos de utilização em python
+
 name = "Red Dead Redemption 2"
 yearLaunch = 2023
 gamePrice = 250.00
 planIncluded = True
 
-print(name)
-print(yearLaunch)
-print(gamePrice)
-print(planIncluded)
+print(name) #String
+print(yearLaunch) #Int
+print(gamePrice) #Float ou Int
+print(planIncluded) #Bloon
 
+
+# Como identificar qual tipo é 
 print(type(name))
 print(type(yearLaunch))
 print(type(gamePrice))
