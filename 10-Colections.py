4dd15d02df79ee437e9fd0d7a689eed372a3f6cc
MatheusHenriqueCIN <159284886+MatheusHenriqueCIN@users.noreply.github.com>
2025-01-31 18:59:16 -0300

    Update 10-Colections.py

--- 10-Colections.py
+++ 10-Colections.py
@@ -1,22 +1,33 @@
-# # É possível criar uma list em um proprío negócio
+x = 0
+list = []
 
-gameFifa = ["Fifa 23", 2022, 120.50, True]
-print(gameFifa)
-
-# Não necessariamente precisa tudo igual
-gamesList = ["Red Dead Redemption","Star Wars Jedi",
-             "Fifa 25","Dark Souls 3","Mario Odyssey"]
-
-print(gamesList)
-
-# 1 - Buscar os dois primeiros itens da Lista | [0:-1]
-print(gamesList[0:2])
-
-# 2 - Buscar o ultimo item da lista | -1
-print(gamesList[-1])
-
-# 3 - Buscar jogos até uma determinada posição
-print(gamesList[:3])
-
-# 4 - Buscar jogos de uma posição em diante | -1 no ultimo então para pegar o 4 jogo tem que ser 4-1 = 3 já que o primeiro é 0
-print(gamesList[1:4])
+while x == 0:
+    y = str(input())
+    if y == "Novo vilão - Muito Perigoso":
+        vilao = str(input())
+        list.insert(0, vilao)
+    elif y == "Novo vilão - Meio perigoso":
+        vilao = str(input())
+        list.append(vilao)
+    elif y == "O que ele está fazendo aqui?":
+        vilao = str(input())
+        list.remove(vilao)
+    elif y == "Vilão mais perigoso do que pensávamos":
+        index1 = int(input())
+        index2 = int(input())
+        list[index1], list[index2] = list[index2], list[index1]
+    elif y == "Que estranho, esses dois vilões… troque-os de lugar":
+        vilao = str(input())
+        vilao2 = str(input())
+        index1 = list.index(vilao)
+        index2 = list.index(vilao2)
+        list[index1], list[index2] = list[index2], list[index1]
+    elif y == "Essa posição não está de acordo, ele nem odeia carecas":
+        vilao = str(input())
+        list.remove(vilao)
+        list.append(vilao)
+    elif y == "Como a lista está ficando?":
+        print(*list, sep=', ')
+    elif y == "Já temos nossa lista de vilões":
+        x += 1
+print(f"O resultado final ficou assim:", *list, sep=", ")
