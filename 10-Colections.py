27900491da43da6939aefefe9f905e26792b27ed
MatheusHenriqueCIN <159284886+MatheusHenriqueCIN@users.noreply.github.com>
2025-01-31 20:15:38 -0300

    Update 10-Colections.py

--- 10-Colections.py
+++ 10-Colections.py
@@ -1,33 +1,82 @@
-x = 0
-list = []
+soma = 0
+listAprimorador = []
+listEmissor = []
+listTransmutador = []
+listManipulador = []
+listConjurador = []
+listEspecialista = []
 
-while x == 0:
-    y = str(input())
-    if y == "Novo vilão - Muito Perigoso":
-        vilao = str(input())
-        list.insert(0, vilao)
-    elif y == "Novo vilão - Meio perigoso":
-        vilao = str(input())
-        list.append(vilao)
-    elif y == "O que ele está fazendo aqui?":
-        vilao = str(input())
-        list.remove(vilao)
-    elif y == "Vilão mais perigoso do que pensávamos":
-        index1 = int(input())
-        index2 = int(input())
-        list[index1], list[index2] = list[index2], list[index1]
-    elif y == "Que estranho, esses dois vilões… troque-os de lugar":
-        vilao = str(input())
-        vilao2 = str(input())
-        index1 = list.index(vilao)
-        index2 = list.index(vilao2)
-        list[index1], list[index2] = list[index2], list[index1]
-    elif y == "Essa posição não está de acordo, ele nem odeia carecas":
-        vilao = str(input())
-        list.remove(vilao)
-        list.append(vilao)
-    elif y == "Como a lista está ficando?":
-        print(*list, sep=', ')
-    elif y == "Já temos nossa lista de vilões":
-        x += 1
-print(f"O resultado final ficou assim:", *list, sep=", ")
+
+qtd_monitores = int(input())
+while soma < qtd_monitores:
+    monitor = input()
+    mudanca = input()
+    
+    if monitor in listAprimorador:
+        listAprimorador.remove(monitor)
+    elif monitor in listConjurador:
+        listConjurador.remove(monitor)
+    elif monitor in listEmissor:
+        listEmissor.remove(monitor)
+    elif monitor in listEspecialista:
+        listEspecialista.remove(monitor)
+    elif monitor in listManipulador:
+        listManipulador.remove(monitor)
+    elif monitor in listTransmutador:
+        listTransmutador.remove(monitor)
+    
+    if mudanca == "O volume da água foi alterado.":
+        listAprimorador.append(monitor)
+        soma += 1
+    elif mudanca == "A cor da água foi alterada.":
+        listEmissor.append(monitor)
+        soma += 1
+    elif mudanca == "O gosto da água foi alterado.":
+        listTransmutador.append(monitor)
+        soma+=1
+    elif mudanca == "A folha se moveu.":
+        listManipulador.append(monitor)
+        soma += 1
+    elif mudanca == "Impureza apareceram na água.":
+        listConjurador.append(monitor)
+        soma += 1
+    else:
+        listEspecialista.append(monitor)
+        soma += 1
+
+if len(listAprimorador) > 0:
+    print(f"Temos um total de {len(listAprimorador)} aprimoradores entre os monitores! Eles são: ", end="")
+    for i, monitor in enumerate(listAprimorador):
+        if i > 0:
+            print(", ", end="")
+        print(monitor, end="")
+if len(listEmissor) > 0:
+    print(f"Temos um total de {len(listEmissor)} emissores entre os monitores! Eles são: ", end="")
+    for i, monitor in enumerate(listEmissor):
+        if i > 0:
+            print(", ", end="")
+        print(monitor, end="")
+if len(listTransmutador) > 0:
+    print(f"Temos um total de {len(listTransmutador)} transmutadores entre os monitores! Eles são: ", end="")
+    for i, monitor in enumerate(listTransmutador):
+        if i > 0:
+            print(", ", end="")
+        print(monitor, end="")
+if len(listManipulador) > 0:
+    print(f"Temos um total de {len(listManipulador)} manipuladores entre os monitores! Eles são: ", end="")
+    for i, monitor in enumerate(listManipulador):
+        if i > 0:
+            print(", ", end="")
+        print(monitor, end="")
+if len(listConjurador) > 0:
+    print(f"Temos um total de {len(listConjurador)} conjuradores entre os monitores! Eles são: ", end="")
+    for i, monitor in enumerate(listConjurador):
+        if i > 0:
+            print(", ", end="")
+        print(monitor, end="")
+if len(listEspecialista) > 0:
+    print(f"Temos um total de {len(listEspecialista)} especialistas entre os monitores! Eles são: ", end="")
+    for i, monitor in enumerate(listEspecialista):
+        if i > 0:
+            print(", ", end="")
+        print(monitor, end="")
